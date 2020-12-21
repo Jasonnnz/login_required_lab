12c8e009093193b4805d109a09e6461dbd4cf686
Jason Zhou <jzhou35@buffalo.edu>
2020-12-21 18:40:30 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,8 @@
+class SecretsController < ApplicationController 
+    
+    before_action :require_logged_in, only: [:show]
+  
+    def show
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :sessions, only: [:create, :destroy, :new]
+  # get 'secrets/:id', to: 'secrets#show', as: 'secret'
+  # delete 'sessions/:id', to: 'sessions#destroy'
+
+  get 'secrets/new'
+
+  root 'application#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  get '/secret' => 'secrets#show'
 end
